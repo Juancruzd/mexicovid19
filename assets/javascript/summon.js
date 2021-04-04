e69e2b71568c80ab9a9557d2035774e4d577b090
Juandedios <juan.cruzd1@outlook.es>
2021-04-03 23:46:33 -0600

    cambios

--- assets/javascript/summon.js
+++ assets/javascript/summon.js
@@ -1,20 +1,20 @@
 // SUMMON
 // URL
-var urlTotal="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_confirmados.csv",
-   urlRecu="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_recuperados.csv",
-   urlActivos="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_casos_activos.csv",
-   urlMuertes="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_muertes.csv",
-   urlMuertesNuevas="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_muertes_nuevas.csv",
-   urlNuevos="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_casos_nuevos.csv",
-   urlUpdateTime="https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/last_updated.csv";
+var urlTotal = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_confirmados.csv",
+  urlRecu = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_recuperados.csv",
+  urlActivos = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_casos_activos.csv",
+  urlMuertes = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_muertes.csv",
+  urlMuertesNuevas = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_muertes_nuevas.csv",
+  urlNuevos = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/series_de_tiempo/covid19_mex_casos_nuevos.csv",
+  urlUpdateTime = "https://raw.githubusercontent.com/Juancruzd/Mexico-datos/master/datos/last_updated.csv";
 
 //totales
-d3.csv(urlTotal,function(data) {
+d3.csv(urlTotal, function (data) {
   var largo = data.length;
-  var tope =largo-1;
-  var div = document.getElementById('totales'); 
-     //div.innerHTML =Number(data[tope]["Nacional"]).toLocaleString('es-us');
-  });
+  var tope = largo - 1;
+  var div = document.getElementById('totales');
+  div.innerHTML = Number(data[tope]["Nacional"]).toLocaleString('es-us');
+});
 /*
 d3.csv(urlRecu,function(data) {
   var largo = data.length;
@@ -31,41 +31,41 @@ d3.csv(urlMuertesNuevas,function(data) {
   });
 */
 
-d3.csv(urlMuertes,function(data) {
+d3.csv(urlMuertes, function (data) {
   var largo = data.length;
-  var tope =largo-1;
-  var div = document.getElementById('muertes'); 
-      //div.innerHTML =Number(data[tope]["Nacional"]).toLocaleString('es-us');
-  });
+  var tope = largo - 1;
+  var div = document.getElementById('muertes');
+  div.innerHTML = Number(data[tope]["Nacional"]).toLocaleString('es-us');
+});
 
-d3.csv(urlNuevos,function(data) {
+d3.csv(urlNuevos, function (data) {
   var largo = data.length;
-  var tope =largo-1;
-  var div = document.getElementById('nuevos'); 
-      //div.innerHTML =Number(data[tope]["Nacional"]).toLocaleString('es-us');
-  });
+  var tope = largo - 1;
+  var div = document.getElementById('nuevos');
+  div.innerHTML = Number(data[tope]["Nacional"]).toLocaleString('es-us');
+});
 
-  d3.csv(urlUpdateTime,function(data) {
-    formatMonth = d3.timeFormat("%b"), //%m
+d3.csv(urlUpdateTime, function (data) {
+  formatMonth = d3.timeFormat("%b"), //%m
     formatDay = d3.timeFormat("%d"),
-    formatHour=d3.timeFormat("%H"),
-    formatMin=d3.timeFormat("%M");
-    var largo = data.length;
-    var tope = largo-1;
-    data[tope]["updated_at"] = new Date(data[tope]["updated_at"]) ;
-    for (var i = 0 ; i <= 6; i++) {
-      var div = document.getElementById('tiempo_actualizacion_'+i);
-       div.innerHTML = "<p class='text-center'><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
-       " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";
-    }
-   /*
-   var div = document.getElementById('tiempo_actualizacion_1');
+    formatHour = d3.timeFormat("%H"),
+    formatMin = d3.timeFormat("%M");
+  var largo = data.length;
+  var tope = largo - 1;
+  data[tope]["updated_at"] = new Date(data[tope]["updated_at"]);
+  for (var i = 0; i <= 6; i++) {
+    var div = document.getElementById('tiempo_actualizacion_' + i);
+    div.innerHTML = "<p class='text-center'><small>Actualizado el: " + formatDay(data[tope]["updated_at"]) + "/" + formatMonth(data[tope]["updated_at"]) +
+      " @ " + formatHour(data[tope]["updated_at"]) + ":" + formatMin(data[tope]["updated_at"]) + "</small></p>";
+  }
+  /*
+  var div = document.getElementById('tiempo_actualizacion_1');
+     div.innerHTML = "<p><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
+     " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";
+   var div = document.getElementById('tiempo_actualizacion_2');
       div.innerHTML = "<p><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
       " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";
-    var div = document.getElementById('tiempo_actualizacion_2');
-       div.innerHTML = "<p><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
-       " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";
-       var div = document.getElementById('tiempo_actualizacion_2');
-       div.innerHTML = "<p><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
-       " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";*/
-    });
+      var div = document.getElementById('tiempo_actualizacion_2');
+      div.innerHTML = "<p><small>Actualizado el: "+formatDay(data[tope]["updated_at"])+ "/"+formatMonth(data[tope]["updated_at"])+
+      " @ "+formatHour(data[tope]["updated_at"])+":"+formatMin(data[tope]["updated_at"])+ "</small></p>";*/
+});
